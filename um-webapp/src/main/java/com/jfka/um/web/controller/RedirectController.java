05d61675fb2324b1f179f2b8ba4eecb9c01132e8
kaffudev <kaffudev@gmail.com>
2021-10-13 11:43:22 +0200

    um-webapp add RedirectController

--- um-webapp/src/main/java/com/jfka/um/web/controller/RedirectController.java
+++ um-webapp/src/main/java/com/jfka/um/web/controller/RedirectController.java
@@ -1,4 +1,47 @@
 package com.jfka.um.web.controller;
 
+import com.jfka.um.utils.UmMappings;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping(method = RequestMethod.GET)
 public class RedirectController {
+
+    public RedirectController() {
+        super();
+    }
+
+    @RequestMapping(value = UmMappings.Singular.PRIVILEGE)
+    public ResponseEntity<Void> privilegeToPrivileges(final HttpServletRequest request) {
+        return singularToPlural(request);
+    }
+
+    @RequestMapping(value = UmMappings.Singular.ROLE)
+    public ResponseEntity<Void> roleToRoles(final HttpServletRequest request) {
+        return singularToPlural(request);
+    }
+
+    @RequestMapping(value = UmMappings.Singular.USER)
+    public ResponseEntity<Void> userToUsers(final HttpServletRequest request) {
+        return singularToPlural(request);
+    }
+
+    // util
+
+    private final ResponseEntity<Void> singularToPlural(final HttpServletRequest request) {
+        final String correctUri = request.getRequestURL()
+                .toString() + "s";
+        final HttpHeaders responseHeaders = new org.springframework.http.HttpHeaders();
+        responseHeaders.add(HttpHeaders.LOCATION, correctUri);
+
+        final ResponseEntity<Void> redirectResponse = new ResponseEntity<>(responseHeaders, HttpStatus.MOVED_PERMANENTLY);
+        return redirectResponse;
+    }
 }
